9b3f757b5015b5117c0e6e04c8a1cd18f0001bfc
Heizo <heizoinside@gmail.com>
2019-07-18 22:09:29 +0300

    написал игру prime

---
+++ src/games/prime.js
@@ -0,0 +1,24 @@
+import randomInteger from '../randomize';
+import run from '../run';
+
+const message = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+const isPrime = (num) => {
+  if (num < 2) return false;
+  const iter = (divisor) => {
+    if (num === divisor) return true;
+    if (num % divisor === 0) return false;
+    return iter(divisor + 1);
+  };
+  return iter(2);
+};
+isPrime(8);
+const game = () => {
+  const num = randomInteger(1, 100);
+  const gameData = {
+    correctAnswer: isPrime(num) ? 'yes' : 'no',
+    question: `${num}`,
+  };
+  return gameData;
+};
+const prime = () => run(game, message);
+export default prime;
